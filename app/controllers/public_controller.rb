1ff0d3cf8849defb719de7d1d06361fc7ea25cb3
Eretzu <m.hemilae@gmail.com>
2017-11-23 16:22:19 +0200

    mio

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,14 +1,16 @@
 class PublicController < ApplicationController
   def home
+    @cities = City.all
+    if(params.has_key?(:date))
     day = Integer(params[:date][:day])
     month = Integer(params[:date][:month])
     date = Date.new(2017, month, day)
     @transports = Transportation.get(params[:departure], params[:arrive], date, params[:sorting])
-    
-    @cities = City.all
-    
-    @TrainCO2 = 100
-    @BusCO2 = 200
-    @PlaneCO2 = 500
+    if(@transports == nil)
+      @transports = []
+    end
+    else
+    @transports = []
+    end 
   end
 end
